575affc4784b779bb8e437848a928ff7d64b84f7
Priyadarshni614 <priyadarshnih.cse2024@citchennai.net>
2026-05-10 20:30:40 +0530

    Fixed route initialization order

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
-
 const rateLimit = require("express-rate-limit");
 
 require("dotenv").config();
+
 const connectDB = require("./config/db");
+
+const authRoutes = require("./routes/authRoutes");
+const taskRoutes = require("./routes/taskRoutes");
+
 const app = express();
+
+// Connect Database
 connectDB();
 
+// Middlewares
 app.use(express.json());
+
 app.use(cors());
+
 app.use(helmet());
 
+// Rate Limiter
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100
@@ -20,17 +30,19 @@ const limiter = rateLimit({
 
 app.use(limiter);
 
+// Test Route
 app.get("/", (req, res) => {
   res.send("API Running");
 });
 
+// Routes
+app.use("/api/auth", authRoutes);
+
+app.use("/api/tasks", taskRoutes);
+
+// Server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-const authRoutes = require("./routes/authRoutes");
-app.use("/api/auth", authRoutes);
-const taskRoutes = require("./routes/taskRoutes");
-app.use("/api/tasks", taskRoutes);
